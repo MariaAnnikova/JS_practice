64ad314d61b7e53f4b89bc9d53f760c537ff3cdc
Maria Annikova <mariaannikova@MacBook-Pro-Maria.local>
2024-02-05 00:14:13 +0200

    JS_homework2

---
+++ JSHW2.js
@@ -0,0 +1,14 @@
+let age = ''
+let userAge = prompt("Будь ласка вкажіть ваш вік", age)
+if (userAge <0 || userAge >100) {
+    alert('введіть число від 0 до 100')};
+let reminder = userAge%10
+if (userAge >=10 && userAge <=20) {
+    userAge = userAge + 'років'}
+else if (reminder ===1) {
+    userAge = userAge + 'рік'}
+else if (reminder >=2 && reminder <=4) {
+    userAge = userAge + 'роки'}
+else {
+    userAge = userAge + 'років'}
+console.log (userAge)
