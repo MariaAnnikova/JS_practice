53f86cdfa978860077f5a0bf1df045d137e8f7e1
Maria Annikova <mariaannikova@MacBook-Pro-Maria.local>
2024-02-15 10:44:07 +0200

    HW5 update

--- JSHOW5.js
+++ JSHOW5.js
@@ -1,11 +1,15 @@
 function powcount (x, y) {
-let pow = x
+    let pow = x
+if (y === 0)
+    return 1;
 for (let i = 1; i<y; i++) { 
     pow*=x;
-    } 
-return pow
+} 
+    return pow;
+
 }
-console.log (powcount (5, 0));
+console.log (powcount (3, 0))
+
 
 
 const result = Math.pow (5,3)  // Так набагато швидше і легше :) - знайшла в інтернеті
