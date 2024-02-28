5930227ed1074767064d36e4b5f5cf80acad1d0f
Maria Annikova <mariaannikova@MacBook-Pro-Maria.local>
2024-02-28 15:15:35 +0200

    HW8.v2

--- JSHW8.js
+++ JSHW8.js
@@ -1,4 +1,4 @@
-const company = {
+/*const company = {
     name: 'Велика Компанія',
     type: 'Головна компанія',
     platform: 'Платформа для продажу квітків',
@@ -88,8 +88,75 @@ const company = {
 console.log(company.findValueByKey(company, 'Велика Компанія')) 
 console.log(company.findValueByKey(company, 'Клієнт 1')) 
 console.log(company.findValueByKey(company, 'Клієнт 1.1'));
-console.log(company.findValueByKey(company,'Клієнт 1.2.3'))
+console.log(company.findValueByKey(company,'Клієнт 1.2.3')) */
 
   
-
+const company = {
+    name: 'Велика Компанія',
+    type: 'Головна компанія',
+    platform: 'Платформа для продажу квітків',
+    selsSolution: 'Рішення для продажу квітків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для родажу квітків',
+            sells: 'Рішення для продажу квітків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квітків',
+                    sells: 'рішення для продажу квітків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квітків',
+                    sells: 'рішення для продажу квітків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квітків',
+                            sells: 'рішення для продажу квітків',
+                        },
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квітків',
+            sells: 'Рішення для продажу квітків'
+        }
+    ]
+}
+function findValueByKey (obj, companyName) {
+    if (obj.name === companyName) {
+        return obj;
+     }
+     if (obj.clients) {
+    for (const client of obj.clients) {
+        const result = findValueByKey (client, companyName) 
+            if (result) {
+                return result;  
+            } 
+        }
+    }
+    if (obj.partners) {
+        for (const partner of obj.partners) {
+            const result = findValueByKey (partner, companyName);
+           if (result) {
+            return result 
+           }
+        }  
+    }
+    return undefined
+}
+console.log (findValueByKey (company, 'Велика Компанія'))
+console.log (findValueByKey (company, 'Клієнт 1'))
+console.log (findValueByKey (company, 'Клієнт 1.1'))
+console.log (findValueByKey (company, 'Клієнт 1.2.3'));
 
